735df2a67004e3992bff2cd443de565768189336
Mindaugas Ulonas <mindegi15@gmail.com>
2022-12-26 06:08:27 +0200

    fixed 'cannot solve map property' issue

--- magicbrainlearning/src/App.js
+++ magicbrainlearning/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.imaginputeUrl).then(
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function(response) {
         console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
