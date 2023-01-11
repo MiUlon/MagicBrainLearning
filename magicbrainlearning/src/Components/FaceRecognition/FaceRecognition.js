df83b1a9803d479d91fd69b40ed70ef8fcada1e8
Mindaugas Ulonas <mulonas@Mindaugass-MacBook-Air.local>
2023-01-12 00:42:01 +0200

    fixingbug

--- magicbrainlearning/src/Components/FaceRecognition/FaceRecognition.js
+++ magicbrainlearning/src/Components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageUrl, setFaceBox }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img id='inputimage' className='center ma4' alt='' src={imageUrl} width='500px' height='auto'></img>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'></img>
                 <div className='bounding-box' style={{top: setFaceBox.topRow, right: setFaceBox.rightCol, bottom: setFaceBox.bottomRow, left: setFaceBox.leftCol}} ></div>
             </div>
         </div>
