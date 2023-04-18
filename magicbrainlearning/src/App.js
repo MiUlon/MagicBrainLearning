c64a2760dc22f6c2517edf7ff790b43251a5e75c
Mindaugas Ulonas <mindegi15@gmail.com>
2023-04-18 20:50:02 +0300

    fixing issues after code review

--- magicbrainlearning/src/App.js
+++ magicbrainlearning/src/App.js
@@ -16,23 +16,25 @@ const app = new Clarifai.App({
   apiKey: '82819b6c8d2d4417abbdebb80e6a3cdc'
 });
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState
   }
 
   onLinkChange = (event) => {
@@ -72,15 +74,16 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count }))
         })
-        this.setFaceBox(this.calculateFaceBox(response))
+        .catch(console.log)
       }
+      this.setFaceBox(this.calculateFaceBox(response))
     })
     .catch(error => console.log(error));
   }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
@@ -114,7 +117,7 @@ class App extends Component {
         </div>
           : (
             route === 'signin'
-            ? <SignIn onRouteChange={this.onRouteChange}/>
+            ? <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
             : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
           )
         }
